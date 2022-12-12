7a2974dba131f10e9f8d56e74b20628443f74dbb
pingzhu <ping.zhu@microfocus.com>
2022-12-12 16:17:02 +0800

    add nacos config

--- payment/src/main/java/com/mf/payment/controller/PaymentController.java
+++ payment/src/main/java/com/mf/payment/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.mf.payment.controller;
 import com.mf.common.Payment;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment")
+@RefreshScope
 public class PaymentController {
 
     @Value("${server.port}")
     private String port;
 
+    @Value("${depart.name}")
+    private String departName;
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Payment> payment(@PathVariable("id") Integer id) {
-        val payment = Payment.builder().id(id).message("支付成功: " + port).build();
+        val payment = Payment.builder().id(id).message(String.format("支付成功: payment{port %s} {department %s}", port, departName) ).build();
         return ResponseEntity.ok(payment);
     }
 }
