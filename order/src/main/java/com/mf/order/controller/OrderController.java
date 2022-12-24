7b53bed277f0f05ee7ffbe3ece0a5fa3c0be8529
pingzhu <ping.zhu@microfocus.com>
2022-12-24 18:38:44 +0800

    add open feign

--- order/src/main/java/com/mf/order/controller/OrderController.java
+++ order/src/main/java/com/mf/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.mf.order.controller;
 
 import com.mf.common.Payment;
+import com.mf.order.feign.PaymentClient;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +20,22 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private PaymentClient paymentClient;
+
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("load-balance")
+    public String getLoadBalance(){
+        val instance = loadBalancerClient.choose("mall-payment");
+        return instance.getHost() + " : " + instance.getPort();
+    }
+
     @GetMapping("/payment/{id}")
     public ResponseEntity<Payment> getPaymentById(@PathVariable("id") Integer id) {
-        val url = String.format("%s/payment/%d", paymentService, id);
-        val payment = restTemplate.getForObject(url, Payment.class);
+        val payment = paymentClient.payment(id);
         return ResponseEntity.ok(payment);
-
     }
 }
---
+++ order/src/main/java/com/mf/order/feign/PaymentClient.java
@@ -0,0 +1,27 @@
+package com.mf.order.feign;
+
+import com.mf.common.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(value = "mall-payment", fallback = PaymentClient.Fallback.class)
+@RequestMapping("payment")
+public interface PaymentClient {
+
+    @GetMapping("/{id}")
+    Payment payment(@PathVariable Integer id);
+
+    @Component
+    @RequestMapping("fallback/payment")
+    class Fallback implements PaymentClient {
+        @Override
+        public Payment payment(Integer id) {
+            Payment payment = new Payment(id, "熔断降级方法返回");
+            return payment;
+
+        }
+    }
+}
